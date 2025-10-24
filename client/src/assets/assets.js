6877be5f6f4ba775d6420dbb5718061188a77e06
Ganga Raghuwanshi <raghuwanshiganga82@gmail.com>
2025-10-25 04:37:17 +0530

    did some changes to navbar

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg'
+// import logo from './logo.svg'
+// import googlePlay from './googlePlay.svg'
 import marvelLogo from './marvelLogo.svg'
-import googlePlay from './googlePlay.svg'
 import appStore from './appStore.svg'
 import screenImage from './screenImage.svg'
 import profile from './profile.png'
 
 export const assets = {
-    logo,
+    // logo,
     marvelLogo,
-    googlePlay,
+    // googlePlay,
     appStore,
     screenImage,
     profile
